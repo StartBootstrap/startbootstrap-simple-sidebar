d9a1cd12bf54d3b50d66ba9b8cf7f485d433b715
David Miller <hello@davidmiller.io>
2017-03-31 23:15:26 -0400

    dev environment

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var pkg = require('./package.json');
+
+// Copy vendor files from /node_modules into /vendor
+// NOTE: requires `npm install` before running!
+gulp.task('copy', function() {
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'))
+
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'))
+
+    gulp.src(['node_modules/tether/dist/js/*.js'])
+        .pipe(gulp.dest('vendor/tether'))
+})
+
+// Default task
+gulp.task('default', ['copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync'], function() {
+    // Reloads the browser whenever HTML or CSS files change
+    gulp.watch('css/*.css', browserSync.reload);
+    gulp.watch('*.html', browserSync.reload);
+});
