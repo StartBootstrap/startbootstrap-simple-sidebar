a8c2cd7137871265c9a4827ad470d0bf74e4124f
David Miller <hello@davidmiller.io>
2017-08-12 00:32:44 -0400

    Bootstrap 4 Beta Update

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,19 @@ var pkg = require('./package.json');
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('vendor/bootstrap'))
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('vendor/jquery'))
 
-    gulp.src(['node_modules/tether/dist/js/*.js'])
-        .pipe(gulp.dest('vendor/tether'))
+  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest('vendor/popper'))
 })
 
 // Default task
@@ -20,16 +25,16 @@ gulp.task('default', ['copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ''
-        },
-    })
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
 })
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync'], function() {
-    // Reloads the browser whenever HTML or CSS files change
-    gulp.watch('css/*.css', browserSync.reload);
-    gulp.watch('*.html', browserSync.reload);
+  // Reloads the browser whenever HTML or CSS files change
+  gulp.watch('css/*.css', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
 });
