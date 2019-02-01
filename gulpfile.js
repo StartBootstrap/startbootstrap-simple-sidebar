e80697429a957d5bcac2deedcfdef9474407ff4e
David Miller <hello@davidmiller.io>
2019-01-31 22:04:39 -0500

    change layout structure to flexbox, update dependencies, update copyright dates, update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
+// Load plugins
+const browsersync = require("browser-sync").create();
+const gulp = require("gulp");
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor', function(cb) {
 
   // Bootstrap
   gulp.src([
@@ -20,22 +20,33 @@ gulp.task('vendor', function() {
     ])
     .pipe(gulp.dest('./vendor/jquery'))
 
-})
+  cb();
 
-// Default task
-gulp.task('default', ['vendor']);
+});
 
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
     server: {
       baseDir: "./"
     }
   });
-});
+  done();
+}
 
-// Dev task
-gulp.task('dev', ['browserSync'], function() {
-  gulp.watch('./css/*.css', browserSync.reload);
-  gulp.watch('./*.html', browserSync.reload);
-});
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./css/*", browserSyncReload);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
+
+gulp.task("default", gulp.parallel('vendor'));
+
+// dev task
+gulp.task("dev", gulp.parallel(watchFiles, browserSync));
