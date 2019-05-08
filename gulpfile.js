a928eaf45410b4f06df053f7aa87c1813c334239
David Miller <hello@davidmiller.io>
2019-05-08 00:55:58 -0400

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,61 @@
+"use strict";
+
 // Load plugins
 const browsersync = require("browser-sync").create();
+const del = require("del");
 const gulp = require("gulp");
-
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
-  cb();
-
-});
+const merge = require("merge-stream");
 
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
       baseDir: "./"
-    }
+    },
+    port: 3000
   });
   done();
 }
 
-// BrowserSync Reload
+// BrowserSync reload
 function browserSyncReload(done) {
   browsersync.reload();
   done();
 }
 
+// Clean vendor
+function clean() {
+  return del(["./vendor/"]);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  // Bootstrap
+  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap'));
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+  return merge(bootstrap, jquery);
+}
+
 // Watch files
 function watchFiles() {
-  gulp.watch("./css/*", browserSyncReload);
+  gulp.watch("./**/*.css", browserSyncReload);
   gulp.watch("./**/*.html", browserSyncReload);
 }
 
-gulp.task("default", gulp.parallel('vendor'));
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor);
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-// dev task
-gulp.task("dev", gulp.parallel(watchFiles, browserSync));
+// Export tasks
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
