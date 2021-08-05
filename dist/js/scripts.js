2b9f049db0590768200d383c3aa4a94741d5b6ef
David Miller <hello@davidmiller.io>
2021-08-05 16:05:30 -0400

    Bootstrap 5.1.0 Update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Simple Sidebar v6.0.2 (https://startbootstrap.com/template/simple-sidebar)
+* Start Bootstrap - Simple Sidebar v6.0.3 (https://startbootstrap.com/template/simple-sidebar)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
 */
